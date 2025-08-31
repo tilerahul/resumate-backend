1479dd29cb122af7ee6fc0d963ac1b4547016102
Rahul Tile <tilerahul8068@gmail.com>
2025-08-31 18:20:54 +0530

    Initial commit

---
+++ src/main/java/com/resumate/resumate_backend/ResumateBackendApplication.java
@@ -0,0 +1,13 @@
+package com.resumate.resumate_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumateBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumateBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/resumate/resumate_backend/ResumateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resumate.resumate_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumateBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
